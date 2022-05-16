e61e5cc07f8f53ff6e93bcd3d06b056bdc9f52de
SilverRooney <silver.rooney.dev@gmail.com>
2022-05-16 11:09:29 -0500

    Initial Commit

---
+++ Edutainment/EdutainmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdutainmentApp.swift
+//  Edutainment
+//
+//  Created by Silver on 5/16/22.
+//
+
+import SwiftUI
+
+@main
+struct EdutainmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
